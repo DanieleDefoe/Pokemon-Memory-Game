1a0fe5d1dba9ed426842914425f8fb18d8d203f4
Abuzar <abuzar28102004@gmail.com>
2023-05-15 14:54:28 +0300

    feat: app github link and icon to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="w-full flex flex-wrap px-5 py-2 text-slate-200 items-center justify-center text-xl bg-slate-800 mt-14">
-      Copyright &copy; {new Date().getFullYear()} DanieleDefoe
+    <footer className="w-full flex flex-wrap px-5 py-3 text-slate-200 items-center gap-3 text-center justify-center text-xl bg-slate-800 mt-14">
+      <h3>Copyright &copy; {new Date().getFullYear()} DanieleDefoe</h3>
+      <a
+        href="https://github.com/DanieleDefoe"
+        rel="noreferrer noopener nofollow"
+        className="github"
+      ></a>
     </footer>
   )
 }
